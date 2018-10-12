db065318b28c7ebe2e3c549b6e32aa56a07c3c09
Joel <jhurtig@academic.rrc.ca>
2018-10-11 19:44:13 -0500

    Added About
    Properly shaming the mtg json person in the about page
    Proper routing to about now in place

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :artists, only: [:index, :show]
-resources :pages, only: [:index]
+#resources :pages, only: [:index]
+
+get 'about', to: 'pages#about', as: 'about'
 
 root to: 'pages#index'
 
